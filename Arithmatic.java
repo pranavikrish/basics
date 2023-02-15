8c977158136bf5e98182381c552a858db4ec76ba
pranavikrish <123976282+pranavikrish@users.noreply.github.com>
2023-02-15 12:15:24 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,23 @@
+public class Arithmetic
+{
+int a=5;
+int b=2;
+int c;
+int add ()
+{
+c=a+b;
+System.out.println("add value is"+c);
+return c;
+}
+int sub()
+{
+c=a-b;
+System.out.println("sub value is"+c);
+return c;
+}
+public static void main(String args[])
+{
+obj.add();
+obj.sub();
+}
+}
